a75ad51b89f0c8a6a11a3f340dee2c3c4d9df523
samriddhi7307 <72978417+samriddhi7307@users.noreply.github.com>
2021-07-07 22:49:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -14,13 +14,36 @@ std_deviation = statistics.stdev(data)
 print("The mean is -> " + str(mean))
 print("The standard deviation is -> " + str(std_deviation))
 
-dataset = []
-for i in range(0, 1000):
-     random_index= random.randint(0,len(data))
-     value = data[random_index]
-     dataset.append(value)
-mean = statistics.mean(dataset)
-std_deviation = statistics.stdev(dataset)
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
 
-print("Mean of sample:- ",mean)
-print("std_deviation of sample:- ",std_deviation)
+def setup():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution :-",mean )
+
+setup()
+
+population_mean = statistics.mean(data)
+print("population mean:- ", population_mean)
+
+def standard_deviation():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+
+    std_deviation = statistics.stdev(mean_list)
+    print("Standard deviation of sampling distribution:- ", std_deviation)
+
+standard_deviation()
